f821279ac77a5cb39e8934669744c6d5bc30d317
Thamir Qadah <qadah.thamir@gmail.com>
2020-05-04 04:44:34 -0400

    fixed some test

--- test/interactive_test.py
+++ test/interactive_test.py
@@ -15,15 +15,18 @@ from ceploy.constants import Provider
 
 def main():
     gcloud = Cloud.make(Provider.GCLOUD, '../gcloud_conf.yml')
-    vm_list = gcloud.list_instances(filter_str="name~'c\\d\\-qcd\\-.'")
+    vm_list = gcloud.list_instances(filter_str="name~'n\\d\\-qcd\\-.'")
     for vm in vm_list:
         print(gcloud.vm_to_str(vm))
 
 
     cmd_out = gcloud.create_instance("n8-qcd-test-1", "n8-qstore", "us-east1-b")
+
+    print(cmd_out)
+
     cmd_out = gcloud.create_instance("n8-qcd-test-2", "n8-qstore", "us-west1-a")
 
-    vm_list = gcloud.list_instances(filter_str="name~'(c|n)?\\d?\\-?qcd\\-.'")
+    vm_list = gcloud.list_instances(filter_str="name~'n\\d\\-qcd\\-.'")
     for vm in vm_list:
         print(gcloud.vm_to_str(vm))
 
